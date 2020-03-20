c5863eb18bf543c47bd17d7044c27fd3aa8a0be1
Dalma0410 <57350184+Dalma0410@users.noreply.github.com>
2020-03-20 16:53:03 +0100

    Bearbeiten Button entfernen

--- Assignment1/SignIn.js
+++ Assignment1/SignIn.js
@@ -1,1 +1,42 @@
 SignIn.js
+var y;
+ 
+function speichern() {
+	// 1. Auslesen der input felder
+	var vornameInputElement = document.getElementById("vorname");
+	console.log(vornameInputElement.value);
+	
+	var nachnameInputElement = document.getElementById("nachname");
+	console.log(nachnameInputElement.value);
+	
+	var telInputElement = document.getElementById("telefonnummer");
+	console.log(telInputElement.value);
+	
+	
+	
+	// 2. In die Tabelle schreiben
+	var tabelle = document.getElementById("userTable");
+	console.log(tabelle);
+	var row = tabelle.insertRow(1);
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+	var cell3 = row.insertCell (2);
+	cell1.innerHTML = vornameInputElement.value;
+	cell2.innerHTML = nachnameInputElement.value;
+	cell3.innerHTML = telInputElement.value;
+	
+	var cell4 = row.insertCell(3);
+	cell4.innerHTML = '<button onclick="loeschen(this)"> X </button>';
+	
+	var cell5 = row.insertCell(4);
+	cell5.innerHTML = '<button onclick="edit(this)"> X </button>';
+	
+}
+
+function loeschen (x) {
+	
+	y =x.parentNode.parentNode;
+    y.parentNode.removeChild(y);
+
+
+}
