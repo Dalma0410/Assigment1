04199af82ffa2d1fff527c47fad0214962f1edcc
Dalma0410 <57350184+Dalma0410@users.noreply.github.com>
2020-03-22 15:10:21 +0100

    Update aboutus.js

--- Assignment1/aboutus.js
+++ Assignment1/aboutus.js
@@ -1,1 +1,10 @@
 aboutus.js
+'use strict';
+document.addEventListener('DOMContentLoaded', function () {
+	document.querySelector('#button')
+		.addEventListener('click', warnung);
+
+	function warnung() {
+		alert('Sie sollten doch nicht drücken!');
+	}
+});
