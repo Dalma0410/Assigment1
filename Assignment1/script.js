8b81a208bbc48d1801244e5ffb815803e58ded7c
Dalma0410 <57350184+Dalma0410@users.noreply.github.com>
2020-03-22 15:39:06 +0100

    Datum Ausgabe

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -1,1 +1,18 @@
 script.js
+
+(function () {
+	function init() {
+		var button = document.getElementById('button');
+		button.addEventListener('click', erzeugeZeitStempel);
+	}
+
+	function erzeugeZeitStempel() {
+		var zeitstempeltext = document.createTextNode(document.lastModified),
+			textZuvor = document.createTextNode('Datum des letzten Updates: ');
+		document.getElementById('absatz')
+			.appendChild(textZuvor);
+		document.getElementById('absatz')
+			.appendChild(zeitstempeltext);
+	}
+	document.addEventListener('DOMContentLoaded', init);
+}());
