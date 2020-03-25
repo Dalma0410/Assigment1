f2925be6c136a0105fe161fa10233cbfc84fdee4
Dalma0410 <57350184+Dalma0410@users.noreply.github.com>
2020-03-25 11:39:16 +0100

    alert

--- Assignment1/aboutus.js
+++ Assignment1/aboutus.js
@@ -1,7 +1,8 @@
 aboutus.js
+
 document.addEventListener('DOMContentLoaded', function () {
-	document.querySelector('button')
-		.addEventListener('click', warnung);
+	//document.querySelector('button')
+	//	.addEventListener('click', warnung);
 
 	function warnung() {
 		alert('Sie sollten doch nicht drücken!');
