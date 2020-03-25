6621f253d0777e54068d4dc4517ca55b1e068981
Dalma0410 <57350184+Dalma0410@users.noreply.github.com>
2020-03-25 13:49:54 +0100

    Datum Button in JS

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -1,22 +1,9 @@
 script.js
 
-(function () {
-	function init() {
-		var button = document.getElementById('button');
-		button.addEventListener('click', erzeugeZeitStempel);
-	}
-
-	function erzeugeZeitStempel() {
-		var zeitstempeltext = document.createTextNode(document.lastModified),
-			textZuvor = document.createTextNode('Datum des letzten Updates: ');
-		document.getElementById('absatz')
-			.appendChild(textZuvor);
-		document.getElementById('absatz')
-			.appendChild(zeitstempeltext);
-	}
-	document.addEventListener('DOMContentLoaded', init);
-}());
-
+function myFunction() {
+	var x = document.lastModified;
+	document.getElementById("demo").innerHTML = x;
+  }
 
 
 	function fensterOeffnen() { 
