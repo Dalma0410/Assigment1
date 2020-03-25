8c28577aeed022cea474f78019bcf7b0bb31d08e
Dalma0410 <57350184+Dalma0410@users.noreply.github.com>
2020-03-25 14:13:47 +0100

    Dark Mode JS

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -1,12 +1,16 @@
 script.js
 
 function myFunction() {
+	var element = document.body;
+	element.classList.toggle("dark-mode");
+ }
+
+function myFunction() {
 	var x = document.lastModified;
 	document.getElementById("demo").innerHTML = x;
   }
 
-
-	function fensterOeffnen() { 
-    window.open('');
+function fensterOeffnen() { 
+	window.open('');
     }
 
