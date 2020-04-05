1194571f14a3fdfff9d15395121cc78223454cc8
Dalma0410 <57350184+Dalma0410@users.noreply.github.com>
2020-04-05 17:01:20 +0200

    Funktion umbennant und Seite hunzugefügt

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -1,11 +1,11 @@
 script.js
 
-function myFunction() {
+function DatumAusgeben() {
 	var x = document.lastModified;
 	document.getElementById("demo").innerHTML = x;
   }
 
 function fensterOeffnen() { 
-	window.open('');
+	window.open('https://www.google.de/');
     }
 
